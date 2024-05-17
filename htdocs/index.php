f7f622370d55590f58d847fdc5e87d38904363ac
YacineMerhoum <yacinemerhoum@gmail.com>
2024-05-17 09:17:35 +0200

    update responsive

--- htdocs/index.php
+++ htdocs/index.php
@@ -41,19 +41,6 @@ $displayPseudo = $preparedRequest->fetchAll(PDO::FETCH_ASSOC);
 </head>
 
 <body>
-  <?php
-
-  // if($id_session){
-  //   echo 'ID de session (récupéré via session_id()) : <br>'
-  //   .$id_session. '<br>';
-  // }
-  // echo '<br><br>';
-  // if(isset($_COOKIE['PHPSESSID'])){
-  //   echo 'ID de session (récupéré via $_COOKIE) : <br>'
-  //   .$_COOKIE['PHPSESSID'];
-  // }
-
-  ?>
 
   <header>
     <nav class="navbar navbar-expand-lg bg-black navbar-dark ">
